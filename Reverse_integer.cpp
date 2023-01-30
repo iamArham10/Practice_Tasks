7c7d3764365212eb54cee1dcb2cca5fab3230f2e
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-01-30 16:12:24 +0500

    Add files via upload

---
+++ Reverse_integer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int reverse(int num);
+main() {
+    cout<<"*********************************************************************************"<<endl;
+    cout<<"                  A program to reverse the given integer                         "<<endl;
+    cout<<"*********************************************************************************"<<endl;
+    int number;
+    cout<<"Enter Your Number: ";
+    cin >> number;
+    cout<<"The reverse of the give number is "<<reverse(number);
+}
+int reverse(int n)
+{
+    int last_digit = 0;
+    int sum = 0;
+    while (n != 0) {
+        last_digit = n % 10;
+        sum = sum*10 + last_digit;
+        n = n / 10;
+        
+        }
+    return sum;
+}
